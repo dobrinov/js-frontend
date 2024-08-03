e647e11c47b7d39a64bd1205458b851cb39f834d
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-03 10:54:45 +0300

    Install React Router

--- src/Application.tsx
+++
@@ -1,3 +0,0 @@
-export function Application() {
-  return <div>Hello World</div>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Application } from './Application'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './Home';
+import { SignIn } from './SignIn';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Application />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
