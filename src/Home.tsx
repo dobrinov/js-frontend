77fe1ed1cb84a26a3f1a1e574652d3c5730996a4
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-03 16:59:28 +0300

    Install Apollo client

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
 import { useRequireAuthenticated } from "./useRequreAuthenticated";
 import { useToken } from "./useToken";
+
+const EXAMPLE_QUERY = gql`
+  query ExampleQuery {
+    viewer {
+      id
+      name
+    }
+  }
+`;
 
 export function Home() {
   const token = useToken();
   useRequireAuthenticated();
+  const { data, loading, error } = useQuery(EXAMPLE_QUERY);
+
+  if (loading) return <div>Loading...</div>;
+  if (error || !data) return <div>Error</div>;
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome, {data.viewer.name}</h1>
       <button type="button" onClick={() => token.clear()}>
         Logout
       </button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Home } from './Home';
-import { SignIn } from './SignIn';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./Home";
+import { SignIn } from "./SignIn";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/graph",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = sessionStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
@@ -15,8 +41,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </React.StrictMode>
+);
