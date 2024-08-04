61c715c9152ed25e17b6abe8f41f71f0a2cb4405
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-05 00:25:33 +0300

    Style the sign in page

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,3 +1,5 @@
+import { XCircleIcon } from "@heroicons/react/20/solid";
+import classNames from "classnames";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToken } from "./useToken";
@@ -46,20 +48,90 @@ export function SignIn() {
   }
 
   return (
-    <form onSubmit={submit}>
-      {error && <div>{error}</div>}
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-      <div>
-        <label>Email</label>
-        <input ref={emailRef} type="email" />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            {error && <Alert text={error} className="mb-5" />}
+            <form className="space-y-6" onSubmit={submit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    ref={emailRef}
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    ref={passwordRef}
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
+    </>
+  );
+}
 
-      <div>
-        <label>Password</label>
-        <input ref={passwordRef} type="password" />
+function Alert({ text, className }: { text: string; className?: string }) {
+  return (
+    <div
+      className={classNames(
+        "border-l-4 border-red-400 bg-red-50 p-4",
+        className,
+      )}
+    >
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon aria-hidden="true" className="h-5 w-5 text-red-400" />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm text-red-700">{text}</p>
+        </div>
       </div>
-
-      <button type="submit">Submit</button>
-    </form>
+    </div>
   );
 }
