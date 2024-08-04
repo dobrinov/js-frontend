f0aa40d4173048236259bf605cb1d1e7cca6c5c7
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-04 23:41:07 +0300

    Reset token on HTTP 401

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -25,8 +26,26 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError && "statusCode" in networkError) {
+    const { statusCode } = networkError;
+
+    if (statusCode === 401) {
+      sessionStorage.removeItem("token");
+      window.location.href = "/";
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(errorLink).concat(httpLink),
   cache: new InMemoryCache(),
 });
 
